cb9629b6214e694294a98f6e3346a11db01ae039
Wyattwicks <wyattwicks@gmail.com>
2021-06-13 13:56:20 -0600

    Add initial testing environment for calls

---
+++ spec/requests/api/v1/location_request_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "Location API", type: :request do
+  describe "happy path" do, :vcr do
+    it "returns coordinates for a given location" do 
+    location = "denver, co"
+    
+    end
+  end
