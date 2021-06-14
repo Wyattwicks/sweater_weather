5deb3c39cd4a6ad5b413e9f2eca3b545d37585f4
Wyattwicks <wyattwicks@gmail.com>
2021-06-14 16:46:37 -0600

    Fix picture facade

--- spec/facades/picture_facade_spec.rb
+++ spec/facades/picture_facade_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 describe "Picture Facade", :vcr do
   it "returns a picture object" do
     location = "denver, co"
-    pic = PictureFacade.get_picture(location)
-    expect(picture).to be_a(Picture)
-    expect(picture.id).to eq(nil)
-    expect(picture.credit).to be_a(Hash)
-    expect(picture.description).to be_a(String)
-    expect(picture.image_url).to be_a(String)
-    expect(picture.credit[:source]).to be_a(String)
-    expect(picture.credit[:author]).to be_a(String)
-    expect(picture.credit[:author_url]).to be_a(String)
+    pic = PictureFacade.picture(location)
+    expect(pic).to be_a(Picture)
+    expect(pic.id).to eq(nil)
+    expect(pic.credit).to be_a(Hash)
+    expect(pic.description).to be_a(String)
+    expect(pic.image_url).to be_a(String)
+    expect(pic.credit[:source]).to be_a(String)
+    expect(pic.credit[:author]).to be_a(String)
+    expect(pic.credit[:author_url]).to be_a(String)
 
   end
 end
