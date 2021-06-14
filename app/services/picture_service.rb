f7c9972773ba32c13f195fac0c860b273d37fedc
Wyattwicks <wyattwicks@gmail.com>
2021-06-14 14:31:49 -0600

    Add picture service

---
+++ app/services/picture_service.rb
@@ -0,0 +1,25 @@
+class PictureService
+  class << self
+
+    def get_image(location)
+      response = conn.get("/search/photos") do |req|
+        req.params['query'] = location
+      end
+      parse_data(response)
+    end
+
+
+    private
+
+    def conn 
+      Faraday.new(
+        url: "https://api.unsplash.com",
+        params: { client_id: ENV['UNSPLASH_ACCESS_KEY']}
+      )
+    end
+
+    def parse_data(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+end
