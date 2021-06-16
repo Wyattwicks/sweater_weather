d13294d4c624377601cf532fbc0b8b923773bcbc
Wyattwicks <wyattwicks@gmail.com>
2021-06-16 07:33:42 -0600

    Get 1b postman working

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::BackgroundController < ApplicationController
 
   def index
-    if params[:location] == nil || params[:location].empty?
+    if params[:query] == nil || params[:query].empty?
       render json: { status: 404, message: 'No location given / location not valid' }, status: :not_found
     else
-      picture = BackgroundFacade.picture(location)
-      render json: BackgroundSerializer.new(image)
+      picture = BackgroundFacade.picture(params[:query])
+      render json: BackgroundSerializer.new(picture)
     end
   end
 
